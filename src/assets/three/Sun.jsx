5893ffe344a45c87e5558c39e2343c4e62004624
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-07-19 20:57:25 -0500

    Create Sun wireframe

--- src/assets/three/Sun.jsx
+++ src/assets/three/Sun.jsx
@@ -1,7 +1,16 @@
 import { Canvas } from "@react-three/fiber";
 
 const Sun = () => {
-
+  return (
+    <div>
+      <Canvas camera={{position: [0, 0, 6.5]}}>
+        <mesh>
+          <sphereGeometry args={[4, 15, 15]} />
+          <meshBasicMaterial color="white" wireframe />
+        </mesh>
+      </Canvas>
+    </div>
+  );
 }
 
 export default Sun;
