084aafa4e12291da36074278d4e50fce463eb558
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-07-19 20:48:32 -0500

    Add background gradient and text

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,7 +1,28 @@
+import Sun from "../../assets/three/Sun.jsx";
+
+/**
+ * Hero component that contains animated sun, name, and title
+ */
 const Hero = () => {
-  return(
-    <section aria-label="Hero">Hero</section>
+  return (
+    <section 
+      aria-label="Hero"
+      className="h-screen"
+    >
+      {/* Hero background */}
+      <div className="bg-gradient-to-b from-indigo/25 via-indigo/15 to-eerie bg-center bg-cover bg-no-repeat h-screen absolute inset-0 -z-10" />
+
+      {/* Hero container */}
+      <div className="container mp-default">
+
+        {/* Hero text container */}
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="font-bold text-5xl">Andrew Aquino</h1>
+          <h2 className="font-bold text-2xl">Frontend Developer</h2>
+        </div>
+      </div>
+    </section>
   );
-}
+};
 
 export default Hero;
