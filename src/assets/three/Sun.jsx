29a44c311ecf01e0b54b3de6809491a6beb71bd4
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-07-21 20:54:45 -0500

    Create and animate sun component

--- src/assets/three/Sun.jsx
+++ src/assets/three/Sun.jsx
@@ -1,22 +1,50 @@
-import { Canvas } from "@react-three/fiber";
+import { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 
 /**
- * Sun ray that takes position, rotation, and color as props
- * @param props position, rotation, color
+ * Animated Ray that takes rotation and animate as props
+ * @param props rotation, animate
  */
-const Ray = ({ position, rotation, color }) => {
+const Ray = ({ rotation, animate = true }) => {
+  // Store the current z offset for this ray
+  const rayRef = useRef();
+
+  // Mesh material constants
+  const metalness = 0;
+  const roughness = 1;
+  const color = "#6320ee";
+
+  /**
+   * React Three Fiber hook that exectues every frame,
+   * where the speed is based on the devices' display framerate.
+   * Rotate the ray clockwise about the z-axis if animate is true.
+   */
+  useFrame(() => {
+    if (animate) {
+      rayRef.current.rotation.z -= 0.0005;
+    }
+  });
+
   return (
-    <group position={position} rotation={rotation}>
+    <group rotation={rotation} ref={rayRef}>
       {/* Middle ray */}
       <group>
         <mesh position={[0, 1, 0]}>
           <cylinderGeometry args={[0.15, 0.05, 0.75]} />
-          <meshBasicMaterial color={color} />
+          <meshStandardMaterial
+            color={color}
+            metalness={metalness}
+            roughness={roughness}
+          />
         </mesh>
 
         <mesh position={[0, 1.5, 0]}>
           <coneGeometry args={[0.15, 0.25]} />
-          <meshBasicMaterial color={color} />
+          <meshStandardMaterial
+            color={color}
+            metalness={metalness}
+            roughness={roughness}
+          />
         </mesh>
       </group>
 
@@ -24,12 +52,20 @@ const Ray = ({ position, rotation, color }) => {
       <group>
         <mesh position={[-0.16, 0.92, 0]} rotation={[0, 0, 0.15]}>
           <cylinderGeometry args={[0.05, 0.025, 0.65]} />
-          <meshBasicMaterial color={color} />
+          <meshStandardMaterial
+            color={color}
+            metalness={metalness}
+            roughness={roughness}
+          />
         </mesh>
 
         <mesh position={[-0.21, 1.25, 0]} rotation={[0, 0, 1.75]}>
           <coneGeometry args={[0.09, 0.09]} />
-          <meshBasicMaterial color={color} />
+          <meshStandardMaterial
+            color={color}
+            metalness={metalness}
+            roughness={roughness}
+          />
         </mesh>
       </group>
 
@@ -37,12 +73,20 @@ const Ray = ({ position, rotation, color }) => {
       <group>
         <mesh position={[0.16, 0.92, 0]} rotation={[0, 0, -0.15]}>
           <cylinderGeometry args={[0.05, 0.025, 0.65]} />
-          <meshBasicMaterial color={color} />
+          <meshStandardMaterial
+            color={color}
+            metalness={metalness}
+            roughness={roughness}
+          />
         </mesh>
 
         <mesh position={[0.21, 1.25, 0]} rotation={[0, 0, -1.75]}>
           <coneGeometry args={[0.09, 0.09]} />
-          <meshBasicMaterial color={color} />
+          <meshStandardMaterial
+            color={color}
+            metalness={metalness}
+            roughness={roughness}
+          />
         </mesh>
       </group>
     </group>
@@ -50,32 +94,71 @@ const Ray = ({ position, rotation, color }) => {
 };
 
 /**
- * Animated wireframe sun component for hero
+ * Animated Globe component that takes animate as props
+ * @param props animate
  */
-const Sun = () => {
-  return (
-    <div className="w-full h-full">
-      <Canvas camera={{ position: [0, 0, 3] }}>
-        {/* Wireframe sphere and rays group */}
-        <group scale={[0.7, 0.7, 0.7]}>
-          {/* Wireframe sphere mesh */}
-          <mesh scale={[1.15, 1.15, 1.15]}>
-            <sphereGeometry args={[0.5, 8, 8]} />
-            <meshBasicMaterial color="#6320ee" wireframe />
-          </mesh>
+const Globe = ({ animate = true }) => {
+  // Store the current y offset for globe
+  const globeRef = useRef();
 
-          {/* Sun rays */}
-          <Ray color="#6320ee" />
-          <Ray position={[0, 0, 0]} rotation={[0, 0, -0.85]} color="#6320ee" />
-          <Ray position={[0, 0, 0]} rotation={[0, 0, -1.55]} color="#6320ee" />
-          <Ray position={[0, 0, 0]} rotation={[0, 0, -2.35]} color="#6320ee" />
-          <Ray position={[0, 0, 0]} rotation={[0, 0, -3.15]} color="#6320ee" />
-          <Ray position={[0, 0, 0]} rotation={[0, 0, -3.95]} color="#6320ee" />
-          <Ray position={[0, 0, 0]} rotation={[0, 0, -4.75]} color="#6320ee" />
-          <Ray position={[0, 0, 0]} rotation={[0, 0, -5.55]} color="#6320ee" />
-        </group>
-      </Canvas>
-    </div>
+  // Mesh material constants
+  const metalness = 0;
+  const roughness = 1;
+  const color = "#6320ee";
+
+  /**
+   * React Three Fiber hook that exectues every frame,
+   * where the speed is based on the devices' display framerate.
+   * Rotate the globe clockwise about the y-axis if animate is true.
+   */
+  useFrame(() => {
+    if (animate) {
+      globeRef.current.rotation.y -= 0.0005;
+    }
+  });
+
+  return (
+    <mesh scale={[1.15, 1.15, 1.15]} ref={globeRef}>
+      <sphereGeometry args={[0.5, 16, 16]} />
+      <meshStandardMaterial
+        color={color}
+        metalness={metalness}
+        roughness={roughness}
+        wireframe
+      />
+    </mesh>
+  );
+};
+
+/**
+ * Sun component for hero with animation controls. Animation paused as default.
+ * @param props animate
+ */
+const Sun = ({ animate = false }) => {
+  return (
+    <Canvas
+      camera={{ position: [0, 0, 2.5] }}
+      className="w-full max-w-80 h-full max-h-96 relative -z-10"
+    >
+      <group>
+        {/* Wireframe globe */}
+        <Globe animate={animate} />
+
+        {/* Sun rays, rotated on the z-axis by radians*/}
+        <Ray rotation={[0, 0, 0]} animate={animate} />
+        <Ray rotation={[0, 0, 0.785]} animate={animate} />
+        <Ray rotation={[0, 0, 1.57]} animate={animate} />
+        <Ray rotation={[0, 0, 2.355]} animate={animate} />
+        <Ray rotation={[0, 0, 3.14]} animate={animate} />
+        <Ray rotation={[0, 0, 3.925]} animate={animate} />
+        <Ray rotation={[0, 0, 4.71]} animate={animate} />
+        <Ray rotation={[0, 0, 5.495]} animate={animate} />
+      </group>
+
+      {/* Directional lighting */}
+      <directionalLight color="#f5f5f5" intensity={5} position={[0, 0, -1]} />
+      <ambientLight color="#f5f5f5" intensity={5} />
+    </Canvas>
   );
 };
 
