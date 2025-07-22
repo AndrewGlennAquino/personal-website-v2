bcf73bdb3ccad2687ce63ec22c486f13a7138855
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-07-22 14:41:07 -0500

    Fade in sun with text and animate sun after fade in

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useAnimate, stagger } from "motion/react";
 import Sun from "../../assets/three/Sun.jsx";
 
@@ -6,6 +6,9 @@ import Sun from "../../assets/three/Sun.jsx";
  * Hero component that contains animated sun, greetings, name, and title
  */
 const Hero = () => {
+  // Hold in state when sun animation begins
+  const [animateSun, setAnimateSun] = useState(false);
+
   // Motion hook that control the sequence of animation triggers
   const [scope, animate] = useAnimate();
 
@@ -14,11 +17,22 @@ const Hero = () => {
     const sequence = async () => {
       await animate([
         [
+          "div",
+          { y: [-15, 0], opacity: [0, 1] },
+          { duration: 0.75, ease: "easeOut" },
+        ],
+        [
           "span",
           { y: [-15, 0], opacity: [0, 1] },
-          { delay: stagger(0.25, { startDelay: 0.5 }), duration: 0.75, ease: "easeOut" },
+          {
+            delay: stagger(0.25),
+            duration: 0.75,
+            ease: "easeOut",
+          },
         ],
       ]);
+
+      setAnimateSun(true);
     };
 
     sequence();
@@ -27,23 +41,18 @@ const Hero = () => {
   return (
     <section aria-label="Hero">
       {/* Hero container */}
-      <div className="container h-screen max-h-180 mp-default mt-0 pt-12 flex flex-col items-center gap-4">
+      <div
+        className="container h-screen max-h-180 mp-default mt-0 pt-12 flex flex-col items-center gap-4"
+        ref={scope}
+      >
         {/* Animated sun */}
-        <Sun animate={true} />
+        <Sun animate={animateSun} />
 
         {/* Hero text container */}
-        <div className="flex flex-col gap-2" ref={scope}>
-          <span className="hero-header">
-            Hello!
-          </span>
-
-          <span className="hero-header">
-            My name is Andrew Aquino
-          </span>
-
-          <span className="hero-subtext">
-            And I'm a Frontend Developer
-          </span>
+        <div className="flex flex-col gap-2">
+          <span className="hero-header">Hello!</span>
+          <span className="hero-header">My name is Andrew Aquino</span>
+          <span className="hero-subtext">And I'm a Frontend Developer</span>
         </div>
       </div>
     </section>
