b0009b6d993bf7f483535349536a3bd38e0a2bb4
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-07-23 02:40:28 -0500

    Remove animated sun and add animated gradient background

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,53 +1,54 @@
-import { useState, useEffect } from "react";
-import { useAnimate, stagger } from "motion/react";
-import Sun from "../../assets/three/Sun.jsx";
+import { useEffect } from "react";
+import { motion, useAnimate, stagger } from "motion/react";
 
 /**
- * Hero component that contains animated sun, greetings, name, and title
+ * Hero component that contains greetings, name, and title
  */
 const Hero = () => {
-  // Hold in state when sun animation begins
-  const [animateSun, setAnimateSun] = useState(false);
-
   // Motion hook that control the sequence of animation triggers
   const [scope, animate] = useAnimate();
 
-  // Use effect to play animations in sequence once on mount, then animate sun
+  // Use effect to play animations in sequence once on mount
   useEffect(() => {
     const sequence = async () => {
       await animate([
         [
-          "div",
-          { y: [-15, 0], opacity: [0, 1] },
-          { delay: stagger(0, {startDelay: 0.5}), duration: 0.75, ease: "easeOut" },
-        ],
-        [
           "span",
           { y: [-15, 0], opacity: [0, 1] },
           {
-            delay: stagger(0.25),
+            delay: stagger(0.25, { startDelay: 0.5 }),
             duration: 0.75,
             ease: "easeOut",
           },
         ],
       ]);
-
-      setAnimateSun(true);
     };
 
     sequence();
   }, []);
 
   return (
-    <section aria-label="Hero">
+    <section aria-label="Hero" className="relative overflow-hidden">
+      {/* Animated gradient background */}
+      <motion.div
+        className="bg-gradient-to-br from-eerie via-indigo/25 to-eerie w-[200vw] h-[200vh] absolute inset-0 -z-30"
+        animate={{
+          x: "-50%",
+          y: "-50%",
+          transition: {
+            duration: 10,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse",
+          },
+        }}
+      />
+
       {/* Hero container */}
       <div
-        className="container h-screen max-h-180 mp-default mt-0 pt-12 flex flex-col items-center gap-4"
+        className="container h-screen max-h-180 mp-default mt-0 pt-12 flex flex-col justify-center items-center gap-4"
         ref={scope}
       >
-        {/* Animated sun */}
-        <Sun animate={animateSun} />
-
         {/* Hero text container */}
         <div className="flex flex-col gap-2">
           <span className="hero-header">Hello!</span>
