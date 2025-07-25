99b03488b3a3fdea7017d20fa9e7517c8fec61b0
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-07-25 17:49:55 -0500

    Add header and top and bottom borders

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,7 +1,19 @@
 const About = () => {
   return (
-    <section aria-label="About">About</section>
+    <section
+      aria-label="About"
+      className=" flex justify-center items-center"
+    >
+      {/* About container */}
+      <div className="container border-smoke/5 border-t-2 border-b-2 rounded-4xl h-screen mp-default">
+        {/* About text container */}
+        <div>
+          {/* About header */}
+          <h1>About Me</h1>
+        </div>
+      </div>
+    </section>
   );
-}
+};
 
 export default About;
