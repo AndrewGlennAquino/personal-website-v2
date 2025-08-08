822669196b3c9e80faa0c1d0a666c48a40ccd2c3
Andrew Aquino <andrewglenn.aquino@gmail.com>
2025-08-08 16:15:36 -0500

    Add credit to Icons8.com

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,13 @@
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer>
+      <div className="mp-default py-4">
+        <a target="_blank" href="https://icons8.com">
+          Icons by Icons8
+        </a>
+      </div>
+    </footer>
   );
-}
+};
 
 export default Footer;
